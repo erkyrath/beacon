9bee3fa63cffd39fd443d8919a6f21ba91cf1f74
Andrew Plotkin <zarf@ZarfLent.local>
2023-12-17 19:22:53 -0500

    Factor out math.

--- src/pulser.rs
+++ src/pulser.rs
@@ -7,6 +7,39 @@ pub enum PulseShape {
     SawTooth,
 }
 
+fn samplepulse(shape: &PulseShape, pos: f32) -> f32 {
+    match shape {
+        PulseShape::Flat => 1.0,
+        PulseShape::Square => {
+            if pos >= 0.0 && pos < 1.0 {
+                1.0
+            }
+            else {
+                0.0
+            }
+        },
+        PulseShape::SawTooth => {
+            if pos >= 0.0 && pos < 1.0 {
+                pos
+            }
+            else {
+                0.0
+            }
+        },
+        PulseShape::Triangle => {
+            if pos >= 0.0 && pos < 0.5 {
+                pos * 2.0
+            }
+            else if pos >= 0.5 && pos < 1.0 {
+                (1.0 - pos) * 2.0
+            }
+            else {
+                0.0
+            }
+        },
+    }                
+}
+
 pub struct Pulse {
     pub spaceshape: PulseShape,
     pub timeshape: PulseShape,
@@ -37,15 +70,7 @@ impl Pulser {
                 let pos = (ix as f32) / bufrange;
                 let mut val = 0.0;
                 for pulse in &self.pulses {
-                    let spaceval = if pos >= 0.0 && pos < 0.5 {
-                        pos * 2.0
-                    }
-                    else if pos >= 0.5 && pos < 1.0 {
-                        (1.0 - pos) * 2.0
-                    }
-                    else {
-                        0.0
-                    };
+                    let spaceval = samplepulse(&pulse.spaceshape, pos);
                     val += spaceval;
                 }
                 buf[ix] = val;
