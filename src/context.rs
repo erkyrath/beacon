690c840c6ee13232c5dc7e2151fc326d178fcc1f
Andrew Plotkin <zarf@ZarfLent.local>
2023-12-22 13:49:44 -0500

    Working.

--- src/context.rs
+++ src/context.rs
@@ -9,6 +9,7 @@ use crate::script::{Script, ScriptIndex};
 use crate::op::{Op1Ctx, Op3Ctx};
 use crate::op::{Op1Def, Op3Def};
 use crate::op::{Op1State, Op3State};
+use crate::pulser::{Pulser, PulserState};
 
 pub struct RunContext {
     pub script: Script,
@@ -29,14 +30,35 @@ pub enum ScriptBuffer<'a> {
 
 impl RunContext {
     pub fn new(script: Script, size: usize) -> RunContext {
+        let mut op1s: Vec<Op1Ctx> = Vec::default();
+        let mut op3s: Vec<Op3Ctx> = Vec::default();
+        
+        for op in &script.op1s {
+            let state = match op {
+                Op1Def::Pulser(_pulser) => Op1State::Pulser(PulserState::new()),
+                _ => Op1State::NoState,
+            };
+            op1s.push(Op1Ctx {
+                state: RefCell::new(state),
+                buf: RefCell::new(vec![0.0; size]),
+            });
+        }
+        
+        for _op in &script.op3s {
+            op3s.push(Op3Ctx {
+                state: RefCell::new(Op3State::NoState),
+                buf: RefCell::new(vec![Pix::new(0.0, 0.0, 0.0); size]),
+            });
+        }
+        
         RunContext {
             script: script,
             size: size,
             birth: Instant::now(),
             age: 0.0,
             rng: Rc::new(RefCell::new(SmallRng::from_entropy())),
-            op1s: Vec::default(), //###
-            op3s: Vec::default(), //###
+            op1s: op1s,
+            op3s: op3s,
         }
     }
 
@@ -84,7 +106,6 @@ impl RunContext {
                 ScriptIndex::Op1(val) => {
                     //###self.op1s[*val].tick(self, &self.script.op1s[*val]);
                     let opdef = &self.script.op1s[*val];
-                    let mut _state = self.op1s[*val].state.borrow_mut();
                     let mut buf = self.op1s[*val].buf.borrow_mut();
                     match &opdef {
                         Op1Def::Constant(val) => {
@@ -93,17 +114,16 @@ impl RunContext {
                             }
                         }
 
-                        /*###
                         Op1Def::Pulser(_pulser) => {
-                            if let Op1State::Pulser(pstate) = state {
+                            //let mut state = self.op1s[*val].state.borrow_mut();
+                            if let Op1State::Pulser(pstate) = &mut *self.op1s[*val].state.borrow_mut() {
                                 pstate.tick(self);
-                                pstate.render(self, buf);
+                                pstate.render(self, &mut buf);
                             }
                             else {
                                 panic!("Op1 state mismatch: PulserState");
                             }
                         }
-                        ###*/
                         
                         _ => {
                             panic!("unimplemented Op1");
@@ -113,7 +133,7 @@ impl RunContext {
                 ScriptIndex::Op3(val) => {
                     //###self.op3s[*val].tick(self, &self.script.op3s[*val]);
                     let opdef = &self.script.op3s[*val];
-                    let mut _state = self.op3s[*val].state.borrow_mut();
+                    //let mut _state = self.op3s[*val].state.borrow_mut();
                     let mut buf = self.op3s[*val].buf.borrow_mut();
                     match &opdef {
                         Op3Def::Constant(val) => {
