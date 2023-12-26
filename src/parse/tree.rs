11a68df3c423e0f3bc1b148b658c8f0cc92198ed
Andrew Plotkin <zarf@ZarfLent.local>
2023-12-26 16:16:42 -0500

    Handle hex colors.

--- src/parse/tree.rs
+++ src/parse/tree.rs
@@ -97,6 +97,26 @@ fn labelterm(val: &str) -> (Option<&str>, ParseTerm) {
     if let Ok(float) = term.parse::<f32>() {
         return (label, ParseTerm::Number(float));
     }
+
+    if term.starts_with('$') && term.len() == 4 {
+        if let (Ok(rval), Ok(gval), Ok(bval)) = (
+            u32::from_str_radix(&term[1..2], 16),
+            u32::from_str_radix(&term[2..3], 16),
+            u32::from_str_radix(&term[3..4], 16)
+        ) {
+            return (label, ParseTerm::Color(Pix::new((rval as f32)/15.0, (gval as f32)/15.0, (bval as f32)/15.0)));
+        }
+    }
+    
+    if term.starts_with('$') && term.len() == 7 {
+        if let (Ok(rval), Ok(gval), Ok(bval)) = (
+            u32::from_str_radix(&term[1..3], 16),
+            u32::from_str_radix(&term[3..5], 16),
+            u32::from_str_radix(&term[5..7], 16)
+        ) {
+            return (label, ParseTerm::Color(Pix::new((rval as f32)/255.0, (gval as f32)/255.0, (bval as f32)/255.0)));
+        }
+    }
     
     return (label, ParseTerm::Ident(term.to_string()));
 }
