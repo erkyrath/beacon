ce14dcd91a12cfc1ed19df63d796a8044259cf8e
Andrew Plotkin <zarf@ZarfLent.local>
2023-12-28 11:57:56 -0500

    Op1 invert.

--- src/parse/layout.rs
+++ src/parse/layout.rs
@@ -106,6 +106,18 @@ lazy_static! {
              } as BuildFuncOp1)
         );
         
+        map.insert(
+            "invert",
+            (vec![
+                OpLayoutParam::param("_1", OpLayoutType::Op1),
+            ],
+             |nod: &ParseNode, pmap: &HashMap<String, usize>| -> Result<BuildOp1, String> {
+                 let subop = parse_for_op1(&nod.params.items[pmap["_1"]])?;
+                 let op = Op1Def::Invert(0);
+                 Ok(BuildOp1::new(op).addchild1(subop))
+             } as BuildFuncOp1)
+        );
+        
         map
     };
     
