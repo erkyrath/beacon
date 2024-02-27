a83edc6efc4c9d88a24ca499177061ef5e5feaa1
Andrew Plotkin <zarf@ZarfLent.local>
2024-02-26 19:11:52 -0500

    Try rigging up a cycle fader.

--- src/context/cyclecontext.rs
+++ src/context/cyclecontext.rs
@@ -1,3 +1,7 @@
+use std::mem;
+use std::cell::RefCell;
+
+use crate::pixel::Pix;
 use crate::runner::{Runner, RunContext, RunContextWrap, PixBuffer};
 use crate::clock::CtxClock;
 
@@ -21,13 +25,18 @@ impl CycleRunner {
 pub struct CycleContext {
     runners: Box<Vec<Runner>>,
     interval: f32,
+    fadetime: f32,
     size: usize,
     fixtick: Option<u32>,
     clock: CtxClock,
 
     curindex: usize,
     curchild: Box<RunContextWrap>,
+    lastchild: Option<Box<RunContextWrap>>,
+    lastchange: f32,
     nextchange: f32,
+
+    changebuf: RefCell<Vec<Pix<f32>>>,
 }
 
 impl CycleContext {
@@ -38,13 +47,18 @@ impl CycleContext {
         CycleContext {
             runners: runners,
             interval: interval,
+            fadetime: 1.0,
             size: size,
             fixtick: fixtick,
             clock: CtxClock::new(fixtick),
 
             curindex: 0,
             curchild: Box::new(child),
+            lastchild: None,
+            lastchange: 0.0,
             nextchange: interval,
+
+            changebuf: RefCell::new(vec![Pix::new(0.0, 0.0, 0.0); size]),
         }
     }
 }
@@ -59,7 +73,13 @@ impl RunContext for CycleContext {
             self.nextchange = newage + self.interval;
             self.curindex = (self.curindex+1) % self.runners.len();
             let runner = self.runners[self.curindex].clone();
-            self.curchild = Box::new(runner.build(self.size, self.fixtick));
+            let lastchild = mem::replace(&mut self.curchild, Box::new(runner.build(self.size, self.fixtick)));
+            self.lastchange = newage;
+            self.lastchild = Some(lastchild);
+        }
+        
+        if self.lastchild.is_some() && newage > self.lastchange + self.fadetime {
+            self.lastchild = None;
         }
         
         self.curchild.tick();
@@ -69,9 +89,58 @@ impl RunContext for CycleContext {
         self.clock.age
     }
     
-    fn applybuf<F>(&self, func: F)
+    fn applybuf<F>(&self, mut func: F)
     where F: FnMut(PixBuffer) {
-        self.curchild.applybuf(func);
+        match &self.lastchild {
+            None => self.curchild.applybuf(func),
+            Some(child) => {
+                let pixsize = self.size;
+                /*###{
+                    let mut changebuf = self.changebuf.borrow_mut();
+                    changebuf.fill(Pix::new(0.0, 0.0, 0.0));
+                    self.curchild.applybuf(|pixbuf| {
+                        match pixbuf {
+                            PixBuffer::Buf1(buf) => {
+                                for xpos in 0..pixsize {
+                                    changebuf[xpos].r += buf[xpos];
+                                    changebuf[xpos].g += buf[xpos];
+                                    changebuf[xpos].b += buf[xpos];
+                                }
+                            },
+                            PixBuffer::Buf3(buf) => {
+                                for xpos in 0..pixsize {
+                                    changebuf[xpos].r += buf[xpos].r;
+                                    changebuf[xpos].g += buf[xpos].r;
+                                    changebuf[xpos].b += buf[xpos].r;
+                                }
+                            },
+                        }
+                    });
+                    child.applybuf(|pixbuf| {
+                        match pixbuf {
+                            PixBuffer::Buf1(buf) => {
+                                for xpos in 0..pixsize {
+                                    changebuf[xpos].r += buf[xpos];
+                                    changebuf[xpos].g += buf[xpos];
+                                    changebuf[xpos].b += buf[xpos];
+                                }
+                            },
+                            PixBuffer::Buf3(buf) => {
+                                for xpos in 0..pixsize {
+                                    changebuf[xpos].r += buf[xpos].r;
+                                    changebuf[xpos].g += buf[xpos].r;
+                                    changebuf[xpos].b += buf[xpos].r;
+                                }
+                            },
+                        }
+                    });
+                }###*/
+                {
+                    let changebuf = self.changebuf.borrow();
+                    func(PixBuffer::Buf3(&changebuf));
+                }
+            }
+        }
     }
 
     fn done(&self) -> bool {
