d142e03c1dc0d0a576b59c9a29c7fe9f7f86a275
Andrew Plotkin <zarf@ZarfLent.local>
2026-01-15 20:31:10 -0500

    Paren errors.

--- pyb/compile.py
+++ pyb/compile.py
@@ -284,11 +284,11 @@ class NodeWave(Node):
             case WaveShape.SQRTOOTH:
                 minval = ctx.store_val(self, 'min', mindata)
                 diffval = ctx.store_val(self, 'diff', '(%s-%s)' % (maxdata, minval,))
-                return '(%s+%s*(pow(mod(%s/%s, 1)), 2))' % (minval, diffval, param, perioddata)
+                return '(%s+%s*(pow(mod(%s/%s, 1), 2)))' % (minval, diffval, param, perioddata)
             case WaveShape.SQRDECAY:
                 minval = ctx.store_val(self, 'min', mindata)
                 diffval = ctx.store_val(self, 'diff', '(%s-%s)' % (maxdata, minval,))
-                return '(%s+%s*(pow(1-mod(%s/%s, 1)), 2))' % (minval, diffval, param, perioddata)
+                return '(%s+%s*(pow(1-mod(%s/%s, 1), 2)))' % (minval, diffval, param, perioddata)
             case WaveShape.TRIANGLE:
                 minval = ctx.store_val(self, 'min', mindata)
                 diffval = ctx.store_val(self, 'diff', '(%s-%s)' % (maxdata, minval,))
