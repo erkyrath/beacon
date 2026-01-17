42e837960b82edadf3f569c02158551b9e20a17b
Andrew Plotkin <zarf@ZarfLent.local>
2026-01-17 13:16:29 -0500

    Remove function wrapper.

--- pyb/program.py
+++ pyb/program.py
@@ -92,9 +92,7 @@ class Program:
                 print('%s_pixels = array(pixelCount)' % (nod.id,))
         print()
 
-        print('function atStartup() {')
         ### if nod.buffered and not time-dependent
-        print('}')
         print()
         
         print('export function beforeRender(delta) {')
